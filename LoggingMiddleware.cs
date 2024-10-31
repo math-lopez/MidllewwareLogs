2af1e8151fa7b30ad94ac79e9344bb8a778a749c
math-lopez <mah.dejesus@gmail.com>
2024-10-31 19:22:44 -0300

    init lib

---
+++ LoggingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System;
+
+namespace MeuMiddleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _serviceId;
+
+        public LoggingMiddleware(RequestDelegate next, string serviceId)
+        {
+            _next = next;
+            _serviceId = serviceId;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Log antes de processar a solicitação
+            var logEntrada = new
+            {
+                ServiceId = _serviceId,
+                LogType = "Request",
+                Method = context.Request.Method,
+                Path = context.Request.Path,
+                Timestamp = DateTime.UtcNow
+            };
+            Console.WriteLine(JsonSerializer.Serialize(logEntrada));
+
+            await _next(context);
+
+            // Log após processar a solicitação
+            var logSaida = new
+            {
+                ServiceId = _serviceId,
+                LogType = "Response",
+                StatusCode = context.Response.StatusCode,
+                Timestamp = DateTime.UtcNow
+            };
+            Console.WriteLine(JsonSerializer.Serialize(logSaida));
+        }
+    }
+}
